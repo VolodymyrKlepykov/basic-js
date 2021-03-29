4758383125bc1a7fca2dabd86df652824aea74dc
VolodymyrKlepykov <volodymyr.klepykov@gmail.com>
2021-03-29 14:44:51 +0300

    feat: completed task transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,27 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if(!Array.isArray(arr)){
+    throw 'Error';
+  }
+return arr.reduce((a,b,c) => {
+  if(b === '--discard-next' || b === '--double-prev' || b === '--double-next' || b === '--discard-prev') {
+    return a;
+  }
+  if (arr[c - 1] === '--discard-next') {
+    return a;
+  }
+  if(arr[c - 1] === '--double-next') {
+    a.push(b, b);
+  }else {
+    a.push(b);
+  }
+  if(arr[c + 1] === '--discard-prev') {
+    a.splice(a.length - 1, 1)
+  }
+  if(arr[c + 1] === '--double-prev') {
+    a.push(b)
+  }
+  return a;
+}, []);
 };
